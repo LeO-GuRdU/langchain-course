a51471d2a37d0f9ee014abfb0a39b8fb7558a5b2
leonardo-gurdulich <leogurdulich@gmail.com>
2025-11-13 15:46:33 -0300

    Implementacion de agent con ollama

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
@@ -27,19 +30,38 @@ load_dotenv()
 #     print(f"Searching for: {query}")
 #     return tavily.search(query=query)
 
+class Source(BaseModel):
+    """
+    Schema for a source in the search results
+    """
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """
+    Schema for the agent response
+    """
+    answer: str = Field(description="The agent answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
 groq_api_key = os.getenv("GROQ_API_KEY")
-llm = ChatGroq(
-    model="openai/gpt-oss-20b",
+# llm = ChatGroq(
+#     model="openai/gpt-oss-20b",
+#     temperature=0,
+#     api_key=groq_api_key,
+# )
+
+llm = ChatOllama(
+    model="mistral:latest",
     temperature=0,
-    api_key=groq_api_key
 )
+
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
     result = agent.invoke({"messages":HumanMessage(
-        content="Busca ofertas de empleo en linkedin para desarrollador Langchain en USA, que acepte full remoto y resume los resultados."
+        content="Busca ofertas de empleo en linkedin para ingeniero IA Langchain en USA, que acepte full remoto y resume los  3 mejores resultados"
         )})
     print(f"Agent result: {result}")
 
