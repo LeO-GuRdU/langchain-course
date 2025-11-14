e83ed2819503ea6319084f8a46dd0cd8041e18d9
leonardo-gurdulich <leogurdulich@gmail.com>
2025-11-14 09:44:40 -0300

    Implementar original react agent

--- main.py
+++ main.py
@@ -4,29 +4,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain import hub
+from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
-from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
 
-groq_api_key = os.getenv("GROQ_API_KEY")
+google_api_key = os.getenv("GOOGLE_API_KEY")
 tools = [TavilySearch()]
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+    api_key=google_api_key,
+)
+
+# llm = ChatOllama(
+#     model="mistral:latest",
+#     temperature=0,
+# )
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(llm, tools, prompt=react_prompt)
+agent_executor = AgentExecutor.from_agent_and_tools(agent, tools, verbose=True)
 
 def main():
     print("Hello from langchain-course!")
 
-    # llm = ChatGroq(
-    #     model="openai/gpt-oss-20b",
-    #     temperature=0,
-    #     api_key=groq_api_key
-    # )
+    response = agent_executor.invoke(input={"input": "Search for 3 job postings for ai engineer in Argentina and summarize them."})
+    print("Respuesta del agente:", response)
 
-    llm = ChatOllama(
-        model="mistral:latest",
-        temperature=0,
-    )
 
 if __name__ == "__main__":
     main()
