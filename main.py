13b6b71828de95cf3962bf67380402584221519a
leonardo-gurdulich <leogurdulich@gmail.com>
2025-11-14 12:51:33 -0300

    Cambio a with structured output

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ load_dotenv()
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -29,8 +28,8 @@ llm = ChatGoogleGenerativeAI(
 #     model="mistral:latest",
 #     temperature=0,
 # )
+structured_llm=llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     input_variables=[
         "tool_names",
@@ -39,7 +38,7 @@ react_prompt_with_format_instructions = PromptTemplate(
     ],
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
 ).partial(
-    format_instructions=output_parser.get_format_instructions(),
+    format_instructions="",
 )
 
 agent = create_react_agent(
@@ -52,10 +51,7 @@ agent_executor = AgentExecutor.from_agent_and_tools(agent, tools, verbose=True)
 extract_output = RunnableLambda(
     lambda x: x["output"],
 )
-parse_output = RunnableLambda(
-    lambda x: output_parser.parse(x),
-)
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 def main():
     print("Hello from langchain-course!")
